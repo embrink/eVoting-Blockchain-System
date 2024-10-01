2b5844521c72402a73967f2bcec53eb33a419a25
Ella Brink <embrink@syr.edu>
2024-10-01 09:15:58 -0400

    smart contracts for actors
    admin, auditor, and voter smart contracts outline (user stories, see google doc for detailed code examples, next step turn into pseudocode then flesh out program)

---
+++ contracts/AdminContract.sol
@@ -0,0 +1,12 @@
+
+// Administrator User Stories, should have a function for each
+// As an administrator, I want to see the list of voters that have registered for an election. 
+// As an administrator, I want to be able to set up and run elections such as picking the time voting starts and ends, who can vote, the type of election, and select the candidates to be voted upon.
+// As an administrator, I want to be able to cancel elections in case of any issues. 
+// As an administrator, I want to be able to log in to the system. 
+// As an administrator, I want to be able to authenticate my identity before making changes to the election system so that only authorized accounts can manage the election. 
+// As an administrator, I want to specify voting rules (single vote, ranked choice) so that the election follows the correct voting method. 
+// As an administrator, I want to be notified of any system malfunctions in real time.
+// As an Administrator, I want to be able to view a backlog of system malfunctions and responses.
+// As an administrator, I want to view an election dashboard to view real time vote counts and time stamps of voter submissions.
+// As an administrator, I want to assign roles and permissions to other administrators if needed.
---
+++ contracts/AuditContact.sol
@@ -0,0 +1,8 @@
+// Auditor User Stories:
+// As an auditor, I want to be able to monitor elections in real time. 
+// As an auditor, I want to be able to see the number of votes during an election for each candidate. 
+// As an auditor, I want to be able to view the list of voters and their information.
+// As an auditor, I want to be able to approve votes.
+// As an auditor, I want to access logs and election history so that I can review any potential issues or irregularities in the election process. 
+// As an auditor, I want to be able to export the audit logs (voter authentication, voter counts, system malfunctions for admin that may impact audit, etc.)
+// As an auditor, I want to receive notifications if the voting process or system is compromised.
